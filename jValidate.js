577606ea398e5dfddab2f7e757ac4dbe0cee2aa4
Kameshwaran <kameshwaran1068@yahoo.co.uk>
2014-11-10 23:14:35 +0530

    validation methods written

--- jValidate.js
+++ jValidate.js
@@ -17,33 +17,91 @@ var InvalidElementException = function(){}
 
 //Custom Result Class
 var Result = function(status, errorMessage){
-  return {
-    "status" : status,
-    "message" : errorMessage
-  };
+  this.status  = status;
+  this.message = errorMessage;
+  return this;
 }
 
 var ValidationClass = function(className, errorMessage, validationMethod, validCallback, inValidCallback, priority){
-  return {
-    "className" : className,
-    "errorMessage" : errorMessage,
-    "validationMethod" : validationMethod,
-    "validCallback" : validCallback,
-    "inValidCallback" : inValidCallback
-    "priority" : priority
-  };
+  this.className = className;
+  this.errorMessage = errorMessage;
+  this.validationMethod = validationMethod;
+  this.validCallback = validCallback;
+  this.inValidCallback = inValidCallback;
+  this.priority = priority;
+  return this;
 }
 
 var createValidationClasses = function(){
   var availableValidationClasses = new Array();
-  availableValidationClasses.push(new ValidationClass("required", "This field is required"));
-  availableValidationClasses.push(new ValidationClass("minlength", "This field should have atleast chars"));
-  availableValidationClasses.push(new ValidationClass("date", "Invalid date is entered"));
-  availableValidationClasses.push(new ValidationClass("email", "Invalid email is entered"));
-  availableValidationClasses.push(new ValidationClass("time", "Invalid time is entered"));
-  availableValidationClasses.push(new ValidationClass("url", "Invalid url is entered"));
-  availableValidationClasses.push(new ValidationClass("numeric", "This field should be numeric"));
-    
+  availableValidationClasses.push(new ValidationClass(
+    "required", "This field is required", function(value){
+      if(value == undefined)
+        return false;
+      return value.length > 0;
+    }, null, null, 1 ));
+  availableValidationClasses.push(new ValidationClass(
+    "minlength", 
+    "This field should have atleast chars"
+    function(value, minLength){
+    	return (value.length >= minLength);
+    }, null, null, 2 ));
+  availableValidationClasses.push(new ValidationClass(
+    "date", 
+    "Invalid date is entered",
+    function(value){
+        var pattern = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
+        var segmentedParts = value.match(pattern);
+        if(segmentedParts == null)
+            return false;
+        else {
+            var day = parseInt(segmentedParts[1]);
+            var month = parseInt(segmentedParts[2]);
+            var year = segmentedParts[3];
+            if( day >= 1 && day <= 30 ){
+                if( month >= 1 && month <= 12 ){
+                        if(year.length == 4)
+                            return true;
+                    }
+            }
+            return false;
+        }
+    }, null, null, 2);
+  availableValidationClasses.push(new ValidationClass(
+    "email", 
+    "Invalid email is entered",
+    function(value){
+        var emailPattern = /\S+@\S+\.\S+/;
+        return email.pattern(value);
+    }, null, null, 2));
+  availableValidationClasses.push(new ValidationClass(
+    "time", 
+    "Invalid time is entered",
+    function(value){
+        var timeFormat = /^([0-9]{2})\:([0-9]{2}):([0-9]{2})$/; 
+        if(timeFormat.test(value)){
+            return true;
+        }
+        return false;
+    }, null, null, 2));
+  availableValidationClasses.push(new ValidationClass(
+     "url", 
+     "Invalid url is entered",
+     function(value){
+        try{
+            var url = new URL(value);
+            return true;
+        }
+        catch(exception){
+            return false;
+        }
+     }, null, null, 2));
+  availableValidationClasses.push(new ValidationClass(
+    "numeric", 
+    "This field should be numeric"
+    function(value){
+        return !isNaN(value);
+    }, null, null, 2));
 }
 
 jQuery.prototype.validate = function(){
