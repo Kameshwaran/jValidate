1f76d267e8dfbf77d04c5b8f83fe3d496f4b8585
Kameshwaran <kameshwaran1068@yahoo.co.uk>
2014-11-15 14:49:44 +0530

    callbacks removed and refactored

--- jValidate.js
+++ jValidate.js
@@ -13,7 +13,9 @@ jQuery(document).ready(function(){
 });
 
 //Custom Exception class to throw an exception when validate() is called on non-form element
-var InvalidElementException = function(){}
+var InvalidElementException = function(){
+  this.message = "Invalid element";
+}
 
 //Custom Result Class
 var Result = function(status, errorMessage){
@@ -26,8 +28,6 @@ var ValidationClass = function(className, errorMessage, validationMethod, validC
   this.className = className;
   this.errorMessage = errorMessage;
   this.validationMethod = validationMethod;
-  this.validCallback = validCallback;
-  this.inValidCallback = inValidCallback;
   this.priority = priority;
   return this;
 }
@@ -35,73 +35,82 @@ var ValidationClass = function(className, errorMessage, validationMethod, validC
 var createValidationClasses = function(){
   var availableValidationClasses = new Array();
   availableValidationClasses.push(new ValidationClass(
-    "required", "This field is required", function(value){
-      if(value == undefined)
+    "required", 
+    "This field is required", 
+    function( element ){
+      var value = $(element).val();
+      if ( value == undefined )
         return false;
       return value.length > 0;
-    }, null, null, 1 ));
+    }, 1 ));
   availableValidationClasses.push(new ValidationClass(
     "minlength", 
     "This field should have atleast chars"
-    function(value, minLength){
-    	return (value.length >= minLength);
-    }, null, null, 2 ));
+    function( element, minLength ){
+      var value = $(element).val();
+    	return ( value.length >= minLength );
+    }, 2 ));
   availableValidationClasses.push(new ValidationClass(
     "date", 
     "Invalid date is entered",
-    function(value){
-        var pattern = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
-        var segmentedParts = value.match(pattern);
-        if(segmentedParts == null)
-            return false;
-        else {
-            var day = parseInt(segmentedParts[1]);
-            var month = parseInt(segmentedParts[2]);
-            var year = segmentedParts[3];
-            if( day >= 1 && day <= 30 ){
-                if( month >= 1 && month <= 12 ){
-                        if(year.length == 4)
-                            return true;
-                    }
-            }
-            return false;
-        }
-    }, null, null, 2);
+    function( element ){
+      var value = $(element).val();
+      var pattern = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
+      var segmentedParts = value.match(pattern);
+      if(segmentedParts == null)
+          return false;
+      else{
+          var day = parseInt( segmentedParts[1] );
+          var month = parseInt( segmentedParts[2] );
+          var year = segmentedParts[3];
+          if ( day >= 1 && day <= 30 ){
+              if ( month >= 1 && month <= 12 ){
+                      if (year.length == 4)
+                          return true;
+                  }
+          }
+          return false;
+      }
+    }, 2);
   availableValidationClasses.push(new ValidationClass(
     "email", 
     "Invalid email is entered",
-    function(value){
-        var emailPattern = /\S+@\S+\.\S+/;
-        return email.pattern(value);
-    }, null, null, 2));
+    function( element ){
+      var value = $(element).val();
+      var emailPattern = /\S+@\S+\.\S+/;
+      return email.pattern(value);
+    }, 2));
   availableValidationClasses.push(new ValidationClass(
     "time", 
     "Invalid time is entered",
-    function(value){
-        var timeFormat = /^([0-9]{2})\:([0-9]{2}):([0-9]{2})$/; 
-        if(timeFormat.test(value)){
-            return true;
-        }
-        return false;
-    }, null, null, 2));
+    function( element ){
+      var value = $(element).val();
+      var timeFormat = /^([0-9]{2})\:([0-9]{2}):([0-9]{2})$/; 
+      if(timeFormat.test(value)){
+          return true;
+      }
+      return false;
+    }, 2));
   availableValidationClasses.push(new ValidationClass(
-     "url", 
-     "Invalid url is entered",
-     function(value){
-        try{
-            var url = new URL(value);
-            return true;
-        }
-        catch(exception){
-            return false;
-        }
-     }, null, null, 2));
+    "url", 
+    "Invalid url is entered",
+    function( element ){
+      var value = $(element).val();
+      try{
+        var url = new URL(value);
+        return true;
+      }
+      catch(exception){
+        return false;
+      }
+    }, 2));
   availableValidationClasses.push(new ValidationClass(
     "numeric", 
     "This field should be numeric"
-    function(value){
-        return !isNaN(value);
-    }, null, null, 2));
+    function( element ){
+      var value = $(element).val();
+      return !isNaN(value);
+    }, 2));
 }
 
 jQuery.prototype.validate = function(){
